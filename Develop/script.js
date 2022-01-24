279531d75bc029a56795cec1195c1008aa61205e
Aaron Stokes <stokescomp01@gmail.com>
2022-01-24 11:01:22 -0700

    create functions to generate random characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,24 @@
+//generate functions for password characters
 
+function getNumber() {
+    
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+function getLowercase() {
+    
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+function getUppercase() {
+    
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+function getSymbol() {
+    const symbol = '~!@#%^&*+/{}';
+    return symbol[Math.floor(Math.random() *symbol.length)];
+};
+
+console.log(getSymbol());
 
